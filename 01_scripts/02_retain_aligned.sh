258858334f0110e2f70ea38f2f891110717c2b27
bensutherland <bensutherland7@gmail.com>
2017-11-28 12:49:22 -0800

    Add script to only keep merged reads

---
+++ 01_scripts/02_retain_aligned.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Retain only aligned reads from multiple datafiles at once
+
+# Global variables
+MERGED_FOLDER="03_merged"
+
+# Merging multiple files consecutively
+ls -1 $MERGED_FOLDER/*merged.fq | \
+    perl -pe 's/\_merged\.fq//' | \
+    sort -u | \
+    while read i 
+    do
+        echo $i
+    # Use obigrep to only keep aligned sequences
+        obigrep -p 'mode!="joined"' $i"_merged.fq" > $i"_ali.fq"
+    done
+
+# Move files to merged folder
+#mv $MERGED_FOLDER/*ali.fq $OUTPUT_FOLDER
