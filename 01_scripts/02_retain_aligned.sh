8ac09baa92a26507841c2c3865607f85e3cd99ff
bensutherland <bensutherland7@gmail.com>
2017-11-30 15:58:43 -0800

    Change stripping to keep underscore in i (retain_aligned.sh)

--- 01_scripts/02_retain_aligned.sh
+++ 01_scripts/02_retain_aligned.sh
@@ -6,13 +6,13 @@ MERGED_FOLDER="03_merged"
 
 # Merging multiple files consecutively
 ls -1 $MERGED_FOLDER/*merged.fq | \
-    perl -pe 's/\_merged\.fq//' | \
+    perl -pe 's/merged\.fq//' | \
     sort -u | \
     while read i 
     do
         echo $i
     # Use obigrep to only keep aligned sequences
-        obigrep -p 'mode!="joined"' $i"_merged.fq" > $i"_ali.fq"
+        obigrep -p 'mode!="joined"' $i"merged.fq" > $i"ali.fq"
     done
 
 # Move files to merged folder
